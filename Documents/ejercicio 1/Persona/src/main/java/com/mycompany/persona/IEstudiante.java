4f4070916d29fafd3fe5e813c57bfacffc8d7f08
DiegoJsH <u23231210@utp.edu.pe>
2026-07-10 19:27:53 -0500

    semana17

--- Documents/ejercicio 1/Persona/src/main/java/com/mycompany/persona/IEstudiante.java
+++
@@ -1,6 +0,0 @@
-
-package com.mycompany.persona;
-
-public interface IEstudiante {
-    void mostrarRolEstudiante();
-}
--- Documents/ejercicio 1/Persona/src/main/java/com/mycompany/persona/IProfesor.java
+++
@@ -1,8 +0,0 @@
-
-package com.mycompany.persona;
-
-public interface IProfesor {
-    void mostrarRolProfesor();
-}
-    
-    
